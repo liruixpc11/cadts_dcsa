ec04bf140d9de2e7724d1c47a0cd43f119e6a0ed
stoveg <3034918371@qq.com>
2018-03-21 09:04:25 +0800

    vmfile 修改

--- cadts_dcsa/vmfile/client.py
+++ cadts_dcsa/vmfile/client.py
@@ -1,34 +1,93 @@
 # coding=utf-8
+import json
+import struct
+import urllib2
+import os
+import requests
+import socket
+import sys
+
+import time
+
+from cadts_dcsa.block.utils import encode_header, send_all, receive_all, file_size_string
 
 
 class VmFileClient(object):
     def __init__(self, vm_server):
         self.vm_server = vm_server
+        self.s = socket.socket()
+        self.s.connect((host, port))
 
     def begin_download(self, vm_path, guest_path):
-        download_url = 'http://<HOST>/vm_files/f2395ffe-68f1-48b4-a50c-a5e75fcbd8fa'
+        download_url = send_request(s=self.s, vm_path=vm_path, guest_path=guest_path)
         return download_url
 
     def download(self, url, local_path, progress_listener=None):
+        file_name = url.split('/')[-1]
+        u = urllib2.urlopen(url)
+        file_len = int(u.info().getheaders("Content-Length")[0])
+        recv_size = 0
+        BUF_SIZE = 4096
+        with open(os.path.join(local_path, file_name), 'wb')as f:
+            buf = u.read(BUF_SIZE)
+            # s=socket.socket()
+            while buf:
+                f.write(buf)
+                recv_size += len(buf)
+                buf = u.read(BUF_SIZE)
+                self.report_progress(url, '{:.2f}'.format(recv_size *100.0 / file_len))
+                sys.stdout.write('\r{:.2f}'.format(recv_size *100.0 / file_len))
         # report progress
         progress_listener(self, url, 0.1)
         # on error
         progress_listener(self, url, 0.1, '404')
 
     def cancel(self, url):
-        pass
+        request = {'cancel': True, 'url': url}
+        send_message(self.s,request)
 
     def report_progress(self, url, progress):
-        pass
+        request = {'url': url, 'progress': progress}
+        send_message(self.s,request)
 
+
+def send_request(s, vm_path, guest_path):  # request is a dict type
+    request = {'vm_path': vm_path, 'guest_path': guest_path}
+    header_string = encode_header(request)
+    send_all(s, header_string)
+    # receive response
+    len_string = receive_all(s, 4)
+    if len_string:
+        header_length, = struct.unpack('!I', len_string)
+        json_string = receive_all(s, header_length)
+        response = json.loads(json_string)
+        if 'url' in response:
+            url = response['url']
+            return url
+
+
+def send_message(s, request):
+    header_string = encode_header(request)
+    send_all(s, header_string)
 
 def fetch_file(vm_server, vm_path, local_path, guest_path):
     def on_progress(c, url, progress, error=None):
         c.report_progress(url, progress)
         timeout = False
         if timeout or error:
-            c.cancel(url)
+            # c.cancel(url)
+            pass
 
     client = VmFileClient(vm_server)
     url = client.begin_download(vm_path, guest_path)
     client.download(url, local_path, on_progress)
+
+
+if __name__ == '__main__':
+    vm_server = '127.0.0.1'
+    vm_path = r"H:\shiyan\os\windows7x64\Windows 7 x64.vmx"
+    guest_path = r"C:\Windows\System32\winevt\Logs\Security.evtx"
+    local_path = r"E:\1"
+    host = '127.0.0.1'
+    port = 4444
+    fetch_file(vm_server, vm_path, local_path, guest_path)
--- cadts_dcsa/vmfile/server.py
+++ cadts_dcsa/vmfile/server.py
@@ -1,30 +1,54 @@
+# coding=utf-8
+import json
+import logging
+import struct
+import threading
 import uuid
-
 import os
+import sys
+import time
+from cadts_dcsa.block.server.async_server import FileReceiveProtocol
+from cadts_dcsa.block.utils import encode_header
+from twisted.internet.protocol import Factory, connectionDone
+
+LOG = logging.getLogger(__name__)
+
+STATE_LEN = 0
+STATE_HEADER = 1
 
 
 class VmFileServer(object):
-    def __init__(self, http_host, http_base_dir):
+    def __init__(self, http_host, http_base_dir=u'E:/'):
         self.http_host = http_host
         self.http_base_dir = http_base_dir
         self.files_dir = 'vm_files'
 
     def on_download_request(self, vm_path, guest_path):
-        file_name = uuid.uuid4()
+        file_name = str(uuid.uuid4())
         file_path = os.path.join(self.http_base_dir, self.files_dir, file_name)
         # TODO download file to file_path
-        url = 'http://{}/{}/{}'.format(self.http_host, self.files_dir, file_name)
+        self.download_file(vm_path, guest_path, file_path, guest='SXY', password='1234')
+        url = 'http://{}:8000/{}/{}'.format(self.http_host, self.files_dir, file_name)
         # response url
         # register timeout
         timeout_seconds = max(os.path.getsize(file_path) / (1024 * 1024), 30)
-        self.begin_timeout(timeout_seconds)
+        threading.Thread(target=self.begin_timeout, args=[timeout_seconds, url]).start()
+        # self.begin_timeout(timeout_seconds)
+        return url
 
-    def begin_timeout(self, timeout_seconds):
-        pass
+    def begin_timeout(self, timeout_seconds, url):
+        try:
+            time.sleep(timeout_seconds)
+            self.on_timeout(url)
+        except OSError:
+            pass
 
-    def on_progress(self, url):
-        pass
-
+    def on_progress(self, url,progress):
+        sys.stdout.write('{}'.format(url))
+        sys.stdout.write('\r')
+        sys.stdout.write('{}'.format(progress))
+        if progress=='100.00':
+            self.clear_file(url)
     def on_timeout(self, url):
         self.clear_file(url)
 
@@ -35,3 +59,76 @@ class VmFileServer(object):
         file_name = self.extract_file_name(url)
         if os.path.exists(file_name):
             os.remove(file_name)
+
+    def extract_file_name(self, url):
+        file_name = self.http_base_dir + url.split('/')[-2] + '/' + url.split('/')[-1]
+        return file_name
+
+    def download_file(self, vm_path, guest_path, file_path, guest, password):
+        cmd = r'vmrun -T ws -gu {} -gp {} copyFileFromGuestToHost "{}" "{}" "{}"'.format(guest, password, vm_path,
+                                                                                         guest_path, file_path)
+        os.system(cmd)
+
+
+class VmFileProtocol(FileReceiveProtocol):
+    def __init__(self):
+        FileReceiveProtocol.__init__(self)
+        self.state = STATE_LEN
+        self.buffer = ''
+        self.header_len = 0
+
+    def connectionMade(self):
+        self.state = STATE_LEN
+        LOG.info('Connection from %s', self.transport.getPeer())
+
+    def connectionLost(self, reason=connectionDone):
+        LOG.info('Connection lost %s: %s', self.transport.getPeer(), reason)
+
+    def dataReceived(self, data):
+        if self.state == STATE_LEN:
+            self.buffer = data
+        else:
+            self.buffer += data
+
+        if self.state == STATE_LEN:
+            if len(self.buffer) >= 4:
+                self.header_len = struct.unpack('!I', self.buffer[0:4])[0]
+                self.buffer = self.buffer[4:]
+                self.state = STATE_HEADER
+
+        if self.state == STATE_HEADER:
+            if len(self.buffer) >= self.header_len:
+                header_string = self.buffer[0:self.header_len]
+                header_json = json.loads(header_string)
+                if 'vm_path'in header_json:
+                    vm_path = header_json['vm_path']
+                    guest_path = header_json['guest_path']
+                    url = server.on_download_request(vm_path, guest_path)
+                    # send url
+                    self.transport.write(encode_header({'url': url}))
+                    LOG.debug('Header: %s', header_json)
+                if 'cancel' in header_json:
+                    server.on_cancel(header_json['url'])
+                    LOG.debug('Header: %s', header_json)
+                if 'progress' in header_json:
+                    server.on_progress(header_json['url'],header_json['progress'])
+                self.buffer = self.buffer[self.header_len:]
+                self.state = STATE_LEN
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    if os.name != 'nt':
+        from twisted.internet import epollreactor
+
+        epollreactor.install()
+
+    from twisted.internet import reactor
+
+    http_host = '127.0.0.1'
+    server = VmFileServer(http_host)
+    factory = Factory()
+    factory.protocol = VmFileProtocol
+    reactor.listenTCP(4444, factory)
+    reactor.run()
